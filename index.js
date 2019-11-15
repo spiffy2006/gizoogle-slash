b61f872af6de8e84dbbf1fb24a39de765bd5d585
Jake Cox <jcox@deseretdigital.com>
2019-11-15 15:04:49 -0700

    Doing things

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const querystring = require('querystring')
 
 exports.handler = function(event, context, callback) {
   const params = querystring.parse(event.body);
-  const text = params.text || "hi";
-  translate(text).then(translation => {
+  const text = params.text || event.queryStringParameters.text || "hi";
+  translate(event.queryStringParameters.text).then(translation => {
     callback(null, {
       statusCode: 200,
       body:  translation
